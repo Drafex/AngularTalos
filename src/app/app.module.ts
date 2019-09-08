d7613622e44307a597c86347676d7359ecffa3c3
unknown <camilozv18@gmail.com>
2019-09-08 08:58:52 -0500

    Add Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,14 @@ import {PostService} from './post/post.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CreatePostComponent } from './create-post/create-post.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes =[
+  {path: '', redirectTo:'/post-list',pathMatch:'full'},
+  {path: 'post-list', component:PostComponent},
+  {path: 'post-create', component:CreatePostComponent}
+]
+
 
 @NgModule({
   declarations: [
@@ -24,7 +32,8 @@ import { FormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [PostService],
   bootstrap: [AppComponent]
